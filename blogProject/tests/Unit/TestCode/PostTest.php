1313c306f9e97493b33d65ee8ac6b9655b767cef
shutirtha-roy <64854868+shutirtha-roy@users.noreply.github.com>
2021-09-17 22:23:12 +0600

    Add files via upload

---
+++ blogProject/tests/Unit/TestCode/PostTest.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use GuzzleHttp\Psr7\UploadedFile;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class PostTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function only_logged_in_users_can_see_posts()
+    {
+        $response = $this->get('posts/create')->assertRedirect('/login');
+        //When not logged it redirects to login
+    }
+
+
+    /** @test */
+    public function only_logged_in_users_can_see_like_posts()
+    {
+        $response = $this->get('liked/1')->assertRedirect('/login');
+    }
+
+    /** @test */
+    public function only_logged_in_users_can_see_edit_posts()
+    {
+        $response = $this->get('/posts/edit/1')->assertRedirect('/login');
+    }
+
+
+    /** @test */
+    public function an_admin_can_add_posts(){
+        
+        //$this->withoutMiddleware();
+         
+        $this->withoutMiddleware();
+
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->post('/posts/create', [
+            'title' => 'test',
+            'content' => 'content',
+            'image' => '1628112407-Java (programming language).jpg',
+            'created_at' => '2021-08-06 13:22:22',
+            'updated_at' => '2021-08-06 13:22:22',
+            'user_id' => 2,
+        ]);
+
+        $this->assertCount(1, Post::all());
+    }
+
+    /** @test */
+    public function an_admin_can_add_comment()
+    {
+                 
+        $this->withoutMiddleware();
+        $response = $this->get('/comments/create/1', [
+            'username' => 'fake',
+            'comment' => 'fake',
+            'created_at' => '2021-08-06 13:22:22',
+            'updated_at' => '2021-08-06 13:22:22',
+            'user_id' => 2,
+            'post_id' => 2,
+        ]);
+
+        $this->assertCount(1, Comment::all());
+    }
+
+    /** @test */
+    public function create_posts()
+    {
+        $this->withoutMiddleware();
+
+        $this->actingAs(User::factory()->create());
+
+        $response = $this->get('/posts')->assertOk();
+    }
+
+
+
+
+    
+
+
+
+}
